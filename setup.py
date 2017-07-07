8797e4620a0cab30f438a954055c727b7973ad16
Grzegorz Adamowicz <grzegorz.adamowicz@lemondemon.pl>
2017-07-07 14:30:56 +0200

    Add first version of script

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='rdsdbsnap',
+    version='0.1.1',
+    py_modules=['rdsdbsnap'],
+    install_requires=[
+        'Click',
+        'boto3',
+    ],
+    entry_points='''
+        [console_scripts]
+        rdsdbsnap=rdsdbsnap:cli
+    ''',
+
+    # metadata for upload to PyPI
+    author="Grzegorz Adamowicz",
+    author_email="gadamowicz@gstlt.info",
+    description="Tool for managing Amazon RDS snapshots",
+    license="MIT",
+    keywords="aws rds snapshot cli",
+    url="",
+)
+
